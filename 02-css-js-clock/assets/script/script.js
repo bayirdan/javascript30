50abf173fc7d91927ae2205db43ca9962c61d3f6
Burkay <bayirburkay@gmail.com>
2021-04-20 21:19:16 +0300

    updated

--- 02-css-js-clock/assets/script/script.js
+++ 02-css-js-clock/assets/script/script.js
@@ -6,7 +6,7 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds * 6) + 180;
+  const secondsDegrees = seconds * 6 + 180;
   if (secondsDegrees !== 180) {
     counterSecond.style.transition = "all .05s cubic-bezier(0, 1.51, 1, 1.3)";
   } else {
@@ -15,7 +15,7 @@ function setDate() {
   counterSecond.style.transform = `rotate(${secondsDegrees}deg`;
 
   const minutes = now.getMinutes();
-  const minutesDegress = (minutes * 6) + 180;
+  const minutesDegress = minutes * 6 + 180;
   if (minutesDegress !== 180) {
     counterMinute.style.transition = "all .05s cubic-bezier(0, 1.51, 1, 1.3)";
   } else {
@@ -24,7 +24,7 @@ function setDate() {
   counterMinute.style.transform = `rotate(${minutesDegress}deg)`;
 
   const hours = now.getHours();
-  const hoursDegress = (hours * 30) + 180;
+  const hoursDegress = hours * 30 + 180;
   if (hoursDegress !== 180) {
     counterHour.style.transition = "all .05s cubic-bezier(0, 1.51, 1, 1.3)";
   } else {

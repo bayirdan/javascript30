98b43be73d824f0b1c344d7feb121c3a9e16bf03
Burkay <bayirburkay@gmail.com>
2021-04-20 20:09:20 +0300

    updated style and script

--- 02-css-js-clock/assets/script/script.js
+++ 02-css-js-clock/assets/script/script.js
@@ -1,23 +1,36 @@
-const counterSecond = document.querySelector('.counter-second');
-const counterMinute = document.querySelector('.counter-minute');
-const counterHour = document.querySelector('.counter-hour');
-
+const counterSecond = document.querySelector(".counter-second");
+const counterMinute = document.querySelector(".counter-minute");
+const counterHour = document.querySelector(".counter-hour");
 
 function setDate() {
   const now = new Date();
-  
+
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds * 6) + 90;
+  const secondsDegrees = (seconds * 6) + 180;
+  if (secondsDegrees !== 180) {
+    counterSecond.style.transition = "all .05s cubic-bezier(0, 1.51, 1, 1.3)";
+  } else {
+    counterSecond.style.transition = "all 0s cubic-bezier(0, 1.51, 1, 1.3)";
+  }
   counterSecond.style.transform = `rotate(${secondsDegrees}deg`;
 
   const minutes = now.getMinutes();
-  const minutesDegress = (minutes * 6) + 90;
-  counterMinute.style.transform = `rotate(${minutesDegress}deg)`
+  const minutesDegress = (minutes * 6) + 180;
+  if (minutesDegress !== 180) {
+    counterMinute.style.transition = "all .05s cubic-bezier(0, 1.51, 1, 1.3)";
+  } else {
+    counterSecond.style.transition = "all 0s cubic-bezier(0, 1.51, 1, 1.3)";
+  }
+  counterMinute.style.transform = `rotate(${minutesDegress}deg)`;
 
   const hours = now.getHours();
-  const hoursDegress = (hours * 30) + 90;
-  counterHour.style.transform = `rotate(${hoursDegress}deg)`
-
+  const hoursDegress = (hours * 30) + 180;
+  if (hoursDegress !== 180) {
+    counterHour.style.transition = "all .05s cubic-bezier(0, 1.51, 1, 1.3)";
+  } else {
+    counterSecond.style.transition = "all 0s cubic-bezier(0, 1.51, 1, 1.3)";
+  }
+  counterHour.style.transform = `rotate(${hoursDegress}deg)`;
 }
 
 setInterval(setDate, 1000);

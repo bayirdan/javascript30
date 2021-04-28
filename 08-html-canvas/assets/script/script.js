c0e621bc68bfd52d6eceb09e5b3d06311e249757
Burkay <bayirburkay@gmail.com>
2021-04-28 06:29:23 +0300

    08-html-canvas completed

---
+++ 08-html-canvas/assets/script/script.js
@@ -0,0 +1,47 @@
+const inputs = document.querySelectorAll(".top-bar input");
+const canvas = document.querySelector("#drawing");
+const ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+ctx.strokeStyle = "#000000";
+ctx.lineWidth = 5;
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let direction = true;
+
+function draw(e) {
+  if (!isDrawing) return;
+
+  ctx.beginPath();
+  // Starting Position
+  ctx.moveTo(lastX, lastY);
+
+  //way
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+function changeValue() {
+  if (this.type == "range") ctx.lineWidth = this.value;
+  else {
+    ctx.strokeStyle = this.value;
+  }
+  console.log(this);
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
+
+inputs.forEach((input) => input.addEventListener("change", changeValue));

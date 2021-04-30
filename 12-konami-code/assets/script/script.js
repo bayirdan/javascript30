7e00c58ba07571b53507166274caac5ffe00a8e9
Burkay <bayirburkay@gmail.com>
2021-04-30 13:48:33 +0300

    12-konami-code completed

---
+++ 12-konami-code/assets/script/script.js
@@ -0,0 +1,14 @@
+const keys = [];
+const secretCode =
+  "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba";
+let secret = document.querySelector(".secret-area");
+
+window.addEventListener("keyup", (e) => {
+  keys.push(e.key);
+  keys.splice(-secretCode.length - 1, keys.length - secretCode.length);
+  console.log(keys);
+
+  if (keys.join("").includes(secretCode)) {
+    secret.style.opacity = 1;
+  }
+});
